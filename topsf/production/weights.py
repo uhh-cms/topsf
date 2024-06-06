35d5298325f98e8aca8436d126a2815fc1df643a
Johanna Matthiesen <jomatthi@physnet.uni-hamburg.de>
2024-06-06 11:45:54 +0200

    Minor syntax adaptations.

--- topsf/production/weights.py
+++ topsf/production/weights.py
@@ -6,7 +6,6 @@ Producers related to event weights.
 """
 
 from columnflow.production import Producer, producer
-from columnflow.production.cms.btag import btag_weights
 from columnflow.production.cms.electron import electron_weights
 from columnflow.production.cms.mc_weight import mc_weight
 from columnflow.production.cms.muon import muon_weights
@@ -17,6 +16,7 @@ from topsf.production.normalization import normalization_weights
 from topsf.production.gen_top import top_pt_weight
 from topsf.production.gen_v import vjets_weight
 from topsf.production.l1_prefiring import l1_prefiring_weights
+from topsf.util import has_tag
 
 ak = maybe_import("awkward")
 
@@ -27,24 +27,32 @@ def weights(self: Producer, events: ak.Array, **kwargs) -> ak.Array:
     Main event weight producer (e.g. MC generator, scale factors, normalization).
     """
     if self.dataset_inst.is_mc:
-        # compute electron weights
-        electron_mask = (events.Electron.pt >= 35)
-        events = self[electron_weights](events, electron_mask=electron_mask, **kwargs)
+        if not has_tag("skip_electron_weights", self.config_inst, self.dataset_inst, operator=any):
+            electron_mask = (events.Electron.pt >= 35)
+            events = self[electron_weights](events, electron_mask=electron_mask, **kwargs)
+        if not has_tag("skip_muon_weights", self.config_inst, self.dataset_inst, operator=any):
+            muon_mask = (events.Muon.pt >= 30) & (abs(events.Muon.eta) < 2.4)
+            events = self[muon_weights](events, muon_mask=muon_mask, **kwargs)
 
-        # compute muon weights
-        muon_mask = (events.Muon.pt >= 30) & (abs(events.Muon.eta) < 2.4)
-        events = self[muon_weights](events, muon_mask=muon_mask, **kwargs)
+        # # compute electron weights
+        # electron_mask = (events.Electron.pt >= 35)
+        # events = self[electron_weights](events, electron_mask=electron_mask, **kwargs)
 
-        # compute btag weights
-        jet_mask = (events.Jet.pt >= 100) & (abs(events.Jet.eta) < 2.5)
-        events = self[btag_weights](events, jet_mask=jet_mask, **kwargs)
+        # # compute muon weights
+        # muon_mask = (events.Muon.pt >= 30) & (abs(events.Muon.eta) < 2.4)
+        # events = self[muon_weights](events, muon_mask=muon_mask, **kwargs)
+
+        # # compute btag weights
+        # jet_mask = (events.Jet.pt >= 100) & (abs(events.Jet.eta) < 2.5)
+        # events = self[btag_weights](events, jet_mask=jet_mask, **kwargs)
 
         # compute top pT weights
         if self.dataset_inst.has_tag("is_ttbar"):
             events = self[top_pt_weight](events, **kwargs)
 
-        # compute L1 prefiring weights
-        events = self[l1_prefiring_weights](events, **kwargs)
+        if self.config_inst.x.run == 2:
+            # compute L1 prefiring weights
+            events = self[l1_prefiring_weights](events, **kwargs)
 
         # compute V+jets K factor weights
         if self.dataset_inst.has_tag("is_v_jets"):
@@ -66,15 +74,21 @@ def weights(self: Producer, events: ak.Array, **kwargs) -> ak.Array:
 def weights_init(self: Producer) -> None:
     if getattr(self, "dataset_inst", None) and self.dataset_inst.is_mc:
         # dynamically add dependencies if running on MC
+        if not has_tag("skip_electron_weights", self.config_inst, self.dataset_inst, operator=any):
+            self.uses |= {electron_weights}
+            self.produces |= {electron_weights}
+
+        if not has_tag("skip_muon_weights", self.config_inst, self.dataset_inst, operator=any):
+            self.uses |= {muon_weights}
+            self.produces |= {muon_weights}
+
         self.uses |= {
-            electron_weights, muon_weights, btag_weights,
             normalization_weights, pu_weight, mc_weight,
             l1_prefiring_weights,
             top_pt_weight,
             vjets_weight,
         }
         self.produces |= {
-            electron_weights, muon_weights, btag_weights,
             normalization_weights, pu_weight, mc_weight,
             l1_prefiring_weights,
             top_pt_weight,
