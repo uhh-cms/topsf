f607fc2c1cee5769c0f28bc8ec4f47a60444df2a
jomatthi <82223346+jomatthi@users.noreply.github.com>
2025-06-18 19:36:58 +0200

    Remove 'lepton' category.
    Co-authored-by: Daniel Savoiu <daniel.savoiu@cern.ch>

--- topsf/selection/categories.py
+++ topsf/selection/categories.py
@@ -34,12 +34,3 @@ def sel_1e(self: Categorizer, events: ak.Array, **kwargs) -> ak.Array:
     """Select only events in the electron channel."""
     ch = self.config_inst.get_channel("e")
     return events, (events["channel_id"] == ch.id)
-
-
-# @categorizer(uses={"event", "channel_id"})
-# def sel_lepton(self: Categorizer, events: ak.Array, **kwargs) -> ak.Array:
-#     """Combined electron and muon channel, select events either in muon or electron channel."""
-#     ch_m = self.config_inst.get_channel("mu")
-#     ch_e = self.config_inst.get_channel("e")
-#     mask = (events["channel_id"] == ch_m.id) or (events["channel_id"] == ch_e.id)
-#     return events, mask
