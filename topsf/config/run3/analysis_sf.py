8346d45db879b1d69a13878c155ed46f5a8c2a12
Johanna Matthiesen <jomatthi@physnet.uni-hamburg.de>
2025-08-06 14:51:11 +0200

    Increase n_files of limited configs to 2 in order to test merging tasks.

--- topsf/config/run3/analysis_sf.py
+++ topsf/config/run3/analysis_sf.py
@@ -110,7 +110,7 @@ config_2022_preEE_limited = add_config(
     campaign_run3_2022_preEE_nano_v12.copy(),
     config_name="run3_sf_2022_preEE_nano_v12_limited",
     config_id=1_03_22_21,  # 1: SF 03: Run3 22: year 2: limited stat 1: pre EE
-    limit_dataset_files=1,
+    limit_dataset_files=2,
 )
 
 config_2022_postEE_limited = add_config(
@@ -118,7 +118,7 @@ config_2022_postEE_limited = add_config(
     campaign_run3_2022_postEE_nano_v12.copy(),
     config_name="run3_sf_2022_postEE_nano_v12_limited",
     config_id=1_03_22_22,  # 1: SF 03: Run3 22: year 2: limited stat 2: post EE
-    limit_dataset_files=1,
+    limit_dataset_files=2,
 )
 
 config_2023_preBPix_limited = add_config(
@@ -126,7 +126,7 @@ config_2023_preBPix_limited = add_config(
     campaign_run3_2023_preBPix_nano_v12.copy(),
     config_name="run3_sf_2023_preBPix_nano_v12_limited",
     config_id=1_03_23_21,  # 1: SF 03: Run3 23: year 2: limited stat 1: pre BPix
-    limit_dataset_files=1,
+    limit_dataset_files=2,
 )
 
 config_2023_postBPix_limited = add_config(
@@ -134,7 +134,7 @@ config_2023_postBPix_limited = add_config(
     campaign_run3_2023_postBPix_nano_v12.copy(),
     config_name="run3_sf_2023_postBPix_nano_v12_limited",
     config_id=1_03_23_22,  # 1: SF 03: Run3 23: year 2: limited stat 2: post BPix
-    limit_dataset_files=1,
+    limit_dataset_files=2,
 )
 
 # config with medium limited number of files
